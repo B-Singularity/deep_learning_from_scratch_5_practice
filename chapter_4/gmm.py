a0796d773b184c0c27e2d721db85aa48ccee5248
B-singularity <skil1489@naver.com>
2025-01-31 23:48:15 +0900

    add chapter_4

--- chapter_4/gmm.py
+++ chapter_4/gmm.py
@@ -1,19 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-mus = np.array([[2.0, 54.50],
-                [4.3, 80.0]])
-covs = np.array([[[0.07, 0.44],
-                  [0.44, 33.7]],
-                 [[0.17, 0.94],
-                  [0.94, 36.00 ]]])
-phis = np.array([0.35, 0.65])
 
 def multivariate_normal(x, mu, cov):
     det = np.linalg.det(cov)
-    inv = np.iinfo.inv(cov)
+    inv = np.linalg.inv(cov)
     d = len(x)
-    z = 1 . np.sqrt((2 * np.pi) ** d * det)
+    z = 1 / np.sqrt((2 * np.pi) ** d * det)
     y = z * np.exp((x - mu).T @ inv @ (x - mu) / -2.0)
     return y
 
@@ -23,12 +16,22 @@ def gmm(x, phis, mus, covs):
     for k in range(K):
         phi, mu, cov = phis[k], mus[k], covs[k]
         y += phi * multivariate_normal(x, mu, cov)
+    return y
+
 
 
 xs = np.arange(1, 6, 0.1)
 ys = np.arange(40, 100, 0.1)
 X, Y = np.meshgrid(xs, ys)
 Z = np.zeros_like(X)
+
+mus = np.array([[2.0, 54.50],
+                [4.3, 80.0]])
+covs = np.array([[[0.07, 0.44],
+                  [0.44, 33.7]],
+                 [[0.17, 0.94],
+                  [0.94, 36.00 ]]])
+phis = np.array([0.35, 0.65])
 
 for i in range(X.shape[0]):
     for j in range(X.shape[1]):
@@ -40,3 +43,10 @@ ax1 = fig.add_subplot(1, 2, 1, projection='3d')
 ax1.set_xlabel('x')
 ax1.set_ylabel('y')
 ax1.set_zlabel('z')
+ax1.plot_surface(X, Y, Z, cmap='viridis')
+
+ax2 = fig.add_subplot(1, 2, 2)
+ax2.set_xlabel('x')
+ax2.set_ylabel('y')
+ax2.contour(X, Y, Z)
+plt.show()
